7ee605ba725a239b7f4e012a71221d2a5c484c02
Dmitriy <dyuzef@mail.ru>
2022-07-21 18:59:56 +0300

    add paragraf and button tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
-import { Button, Htag } from "../components/index";
+import { Button, Htag, P, Tag } from "../components/index";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Text</Htag>
-      <Button appearance="primary">ClickClickClick</Button>
-      <Button appearance="ghost">ClickClickClick</Button>
+      <Button appearance="primary" arrow="down">ClickClickClick</Button>
+      <Button appearance="ghost" arrow="right">ClickClickClick</Button>
+      <P size="l">Big TEXT</P> 
+      <P> TEXT</P>
+      <P size="s">Small TEXT</P>
+      <Tag size="s">Small</Tag>
+      <Tag size="m" color="red">Small</Tag>
+      <Tag size="s" color="green">Small</Tag>
+      <Tag size="m" color="grey">Small</Tag>
     </>
   );
 }
