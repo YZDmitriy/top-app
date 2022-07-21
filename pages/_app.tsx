69401f3c6d4c832e19d61d38380925d181044e78
Dmitriy <dyuzef@mail.ru>
2022-07-21 16:25:44 +0300

    add components header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import { AppProps } from '../node_modules/next/dist/shared/lib/router/router';
+import Head from '../node_modules/next/head';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>My Top App</title>
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;700&display=swap'
+          rel='stylesheet'
+        ></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
