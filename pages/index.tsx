9740538830ff7accaa7255ab9f1365ced15bae68
Dmitriy <dyuzef@mail.ru>
2022-07-21 17:33:03 +0300

    add button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import { Htag } from "../components/index";
+import { Button, Htag } from "../components/index";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Text</Htag>
+      <Button appearance="primary">ClickClickClick</Button>
+      <Button appearance="ghost">ClickClickClick</Button>
     </>
   );
 }
