a5d15f3eae847374de51704f171894f0df96aa2c
Dmitriy <dyuzef@mail.ru>
2022-07-28 17:27:27 +0300

    add hoc and mpre component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
-import { Button, Htag, P, Tag } from "../components/index";
+import { useState } from "react";
+import { Button, Htag, P, Rating, Tag } from "../components/index";
+import { withLayout } from "../layout/Layout";
 
 export default function Home(): JSX.Element {
+
+  const [rating, setRating] = useState<number>(4)
+
   return (
     <>
       <Htag tag="h1">Text</Htag>
-      <Button appearance="primary" arrow="down">ClickClickClick</Button>
+      <Button appearance="primary" arrow="down">ClickClickClick</Button> 
       <Button appearance="ghost" arrow="right">ClickClickClick</Button>
       <P size="l">Big TEXT</P> 
       <P> TEXT</P>
@@ -13,6 +18,9 @@ export default function Home(): JSX.Element {
       <Tag size="m" color="red">Small</Tag>
       <Tag size="s" color="green">Small</Tag>
       <Tag size="m" color="grey">Small</Tag>
+      <Rating rating={rating} isEditable setRating={setRating}/>
     </>
   );
 }
+
+export default withLayout(Home);
